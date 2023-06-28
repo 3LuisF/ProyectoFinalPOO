f3e6b29e1836d20bf26db7e865bcb661ee0bb1f7
Luis F Montoya <lfmontoya03@gmail.com>
2023-06-27 21:24:44 -0500

    primera emtrega

---
+++ src/main/java/com/mycompany/proyectofinalpoo/ProyectoFinalPOO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalpoo;
+
+/**
+ *
+ * @author Luis F Montoya
+ */
+public class ProyectoFinalPOO {
+
+    public static void main(String[] args) {
+        Principal  principalGUI = new Principal();
+        principalGUI.setVisible(true);
+        principalGUI.setLocationRelativeTo(null);
+        
+    }
+}
